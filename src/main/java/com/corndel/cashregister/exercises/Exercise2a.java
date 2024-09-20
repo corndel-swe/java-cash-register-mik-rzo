8cdfed37f6b32666f4f35f925f297e646a0ea68b
mik-rzo <michael.panong3@gmail.com>
2024-09-20 10:58:48 +0100

    solves 2a

--- src/main/java/com/corndel/cashregister/exercises/Exercise2a.java
+++ src/main/java/com/corndel/cashregister/exercises/Exercise2a.java
@@ -8,7 +8,12 @@ public class Exercise2a {
    * Given a list of recipes, return the number of vegetarian recipes in the list.
    */
   public static int countVeggies(List<Recipe> recipes) {
-    // TODO
-    return 0;
+    int count = 0;
+    for (Recipe recipe : recipes) {
+      if (recipe.isVegetarian) {
+        count++;
+      }
+    }
+    return count;
   }
 }
